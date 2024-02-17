7a95c6ebe460106d5794d362d5278165de29cb0e
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2024-02-18 01:09:48 +0530

    Integrated GetCategories Api and apply animations ..

---
+++ src/constanats/index.js
@@ -0,0 +1,45 @@
+export const categoryData =[
+    {
+        name:'Starter',
+        image:'https://www.themealdb.com/images/category/starter.png'
+    },
+    {
+        name:'Beef',
+        image:'https://www.themealdb.com/images/category/beef.png'
+    },
+    {
+        name: 'Pasta',
+        image:'https://www.themealdb.com/images/category/pasta.png'
+    },
+    {
+        name:'Vegetarian',
+        image:'https://www.themealdb.com/images/category/vegetarian.png'
+    },
+    {
+        name: 'Dessert',
+        image:'https://www.themealdb.com/images/category/dessert.png'
+    }
+]
+
+export const mealData =[
+    {
+        name: 'Shakshuka',
+        image:'https://www.themealdb.com/images/media/meals/g373701551450225.jpg'
+    },
+    {
+        name: 'Teriyaki Chicken Casserole',
+        image:'https://www.themealdb.com/images/media/meals/wvpsxx1468256321.jpg'
+    },
+    {
+        name: 'Dal fry',
+        image:'https://www.themealdb.com/images/media/meals/wvpsxx1468256321.jpg'
+    },
+    {
+        name: 'Apple Frangipan Tart',
+        image:'https://www.themealdb.com/images/media/meals/wxywrq1468235067.jpg'
+    },
+    {
+        name: 'Potato Gratin with Chicken',
+        image:'https://www.themealdb.com/images/media/meals/qwrtut1468418027.jpg'
+    },
+]
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,34 @@
 import {Image, ScrollView, Text, TextInput, View} from "react-native";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {StatusBar} from "expo-status-bar";
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from "react-native-responsive-screen";
+import {heightPercentageToDP as hp} from "react-native-responsive-screen";
 import {BellIcon, MagnifyingGlassIcon} from "react-native-heroicons/outline";
+import Categories from "../components/categories";
+import axios from "axios"
+import {Recipes} from "../components/recipes";
 
 export default function HomeScreen() {
-    return (
-        <View className="flex-1  bg-white">
+    const [activeCategory, setActiveCategory] = useState('Brief')
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        getCategories();
+    }, [])
+
+    const getCategories = async () => {
+        try {
+            const response = await axios.get('https://themealdb.com/api/json/v1/1/categories.php')
+            //console.log('got categories ',response.data)
+            if (response && response.data) {
+                setCategories(response.data.categories)
+            }
+        } catch (err) {
+            console.log("error", err.message)
+        }
+    };
+
+
+    return (<View className="flex-1  bg-white">
             <StatusBar style={"dark"}/>
             <ScrollView
                 showsVerticalScrollIndicator={false}
@@ -16,18 +38,20 @@ export default function HomeScreen() {
 
                 {/* avatar and notification icon*/}
                 <View className="mx-4 flex-row justify-between items-center mb-2">
-                    <Image source={require('../../assets/images/avatar.png')} style={{height: hp(5.2), width: hp(5.5)}}/>
+                    <Image source={require('../../assets/images/avatar.png')}
+                           style={{height: hp(5.5), width: hp(5.5)}}/>
                     <BellIcon size={hp(4)} color="gray"/>
                 </View>
 
                 {/*greeting section */}
                 <View className="mx-4 space-y-2">
-                    <Text style={{fontSize:hp(1.7)}} className="text-neutral-600">Hello John</Text>
+                    <Text style={{fontSize: hp(1.7)}} className="text-neutral-600">Hello John</Text>
 
                     <View className="">
-                        <Text style={{fontSize:hp(3.8)}} className="font-semibold text-neutral-600 ">Make your own food</Text>
+                        <Text style={{fontSize: hp(3.8)}} className="font-semibold text-neutral-600 ">Make your own
+                            food</Text>
                     </View>
-                    <Text style={{fontSize:hp(3.8)}} className="font-semibold text-neutral-600 ">
+                    <Text style={{fontSize: hp(3.8)}} className="font-semibold text-neutral-600 ">
                         Stay at <Text className="text-amber-400">home</Text>
                     </Text>
                 </View>
@@ -35,20 +59,33 @@ export default function HomeScreen() {
                 {/*Search Bar */}
                 <View className="mx-4 flex-row items-center rounded-full bg-black/5 p-[6px]">
                     <TextInput
-                      placeholder={'Search any recipe'}
-                      placeholderTextColor={'gray'}
-                      style={{fontSize:hp(1.7)}}
-                      className={'flex-1 text-base mb-1 pl-3 tracking-wider'}
+                        placeholder={'Search any recipe'}
+                        placeholderTextColor={'gray'}
+                        style={{fontSize: hp(1.7)}}
+                        className={'flex-1 text-base mb-1 pl-3 tracking-wider'}
                     />
 
-                   <View className={"bg-white rounded-full p-3"}>
-                       <MagnifyingGlassIcon size={hp(2.5)} strokWidth={3} color={"gray"}/>
-                   </View>
-
+                    <View className={"bg-white rounded-full p-3"}>
+                        <MagnifyingGlassIcon size={hp(2.5)} strokWidth={3} color={"gray"}/>
+                    </View>
                 </View>
 
+                {/*Categories*/}
+                <View>
+                    {categories.length > 0 &&
+                        <Categories
+                            categories={categories}
+                            activeCategory={activeCategory}
+                            setActiveCategory={setActiveCategory}/>
+                    }
+                </View>
+
+                {/*recipes*/}
+
+                <View>
+                    <Recipes categories={categories}/>
+                </View>
             </ScrollView>
 
-        </View>
-    );
+        </View>);
 }
