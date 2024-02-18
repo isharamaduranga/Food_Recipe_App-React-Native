d0bf9f4ce635e9209ba3ff0f57cb1d11a2c3db02
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2024-02-18 18:11:31 +0530

    content updated with recipe details tab | food instructions sec

--- src/screens/RecipeDetailsScreen.js
+++ src/screens/RecipeDetailsScreen.js
@@ -202,6 +202,19 @@ export default function RecipeDetailsScreen(props) {
                     </View>
                 </Animated.View>
 
+                {/*instructions*/}
+                <Animated.View  entering={FadeInDown.delay(300).duration(700).springify().damping(12)} className={"space-y-4"}>
+                    <Text style={{fontSize: hp(2.5)}} className={"font-bold flex-1 text-neutral-700"}>
+                        Instructions
+                    </Text>
+
+                    <Text style={{fontSize:hp(1.6)}} className={"text-neutral-700"}>
+                        {
+                            meal?.strInstructions
+                        }
+                    </Text>
+                </Animated.View>
+
 
 
             </View>
